b59f92d6077d50aef77c1ca9e3c96ca8347b771a
Daniel Rodriguez <44824788+DanRod1999@users.noreply.github.com>
2018-11-07 13:15:06 -0500

    Add files via upload

---
+++ area_calculator.py
@@ -0,0 +1,11 @@
+import random
+from random import shuffle
+
+#triangle Calc
+base = int(input("Please Insert a base:"))
+height = int(input("Please Insert a Height:"))
+
+area = base * height
+print("ur area iz ",area)
+
+
